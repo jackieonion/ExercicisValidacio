bb167e48097f75313ca5d37e1751320f16ee5764
Sebastian Fornelli <cf18sebastian.fornelli@iesjoandaustria.org>
2019-05-13 16:48:10 +0200

    Exercici 7

--- Punt.java
+++ Punt.java
@@ -1,10 +1,10 @@
  public class Punt {
-     private int x, y;   // coordenades del punt
-     public Punt() { this(0, 0); }
-     public Punt(int x, int y) { this.x=x; this.y=y; }
-     public int getX() { return x; }
-     public int getY() { return y; }
-     public void setX(int x) { this.x = x; }
-     public void setY(int y) { this.y = y; }
-     public void suma(Punt p) { x+=p.x; y+=p.y; }
+     private int[] coord = new int[2];   // coordenades del punt
+     public Punt() { this.coord[0] = 0; this.coord[1] = 0; }
+     public Punt(int x, int y) { this.coord[0]=x; this.coord[1]=y; }
+     public int getX() { return coord[0]; }
+     public int getY() { return coord[1]; }
+     public void setX(int x) { this.coord[0] = x; }
+     public void setY(int y) { this.coord[1] = y; }
+     public void suma(Punt p) { coord[0]+=p.coord[0]; coord[1]+=p.coord[1]; }
  }
